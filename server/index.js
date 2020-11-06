bb3f37caff87133969094b5849cc85b0ea2791d5
Forace Avant <themattavant@gmail.com>
2020-11-06 16:37:34 -0700

    created Join component with css and js files

--- server/index.js
+++ server/index.js
@@ -2,12 +2,24 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
+
+const router = require('./router');
 
 //Creates a server instance of socket.io for real time communication
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket)=> {
+    console.log('New connection has been made!');
+
+    socket.on('disconnect', () => {
+        console.log('User has left :(');
+    })
+})
+
+app.use(router);
 
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
