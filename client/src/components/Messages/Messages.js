d86de107dfa42a7d101695c23bb0c0e7405df5cf
Forace Avant <themattavant@gmail.com>
2020-11-06 20:11:21 -0700

    Added Emojis to text area

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -5,8 +5,8 @@ import Message from '../Message/Message'
 
 import './Messages.css';
 
-const Messages = ({ messages, message}) => (
-    <ScrollToBottom>
+const Messages = ({ messages, name}) => (
+    <ScrollToBottom className="messages">
         {messages.map((message, i) => <div key={i}><Message message={message} name={name} /></div>)}
     </ScrollToBottom>
     
